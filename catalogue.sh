3fc3e2c7438ba120881f7863524a81f6ecf8a7e5
Roman Reings <Rajkumar>
2023-12-03 11:54:02 +0530

    added mongodb

--- catalogue.sh
+++ catalogue.sh
@@ -69,7 +69,7 @@ VALIDATE $? "enable catalogue"
 systemctl start catalogue
 VALIDATE $? "start catalogue"
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/Shell/mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "copying mongo.repo"
 
 yum install mongodb-org-shell -y
