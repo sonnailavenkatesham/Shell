5db4bf1744a2548a77e26572926f97ed29cc5f26
Roman Reings <Rajkumar>
2023-12-03 10:46:41 +0530

    added mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 DATE=$(date +%x)
 USER_ID=$(id -u)
-LOG_FILE=/home/centos/Shell/tmp/$0-$DATE-log
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -9,33 +8,33 @@ N="\e[0m"
 if [ $USER_ID -ne 0 ]
 then
     echo -e "$R ERROR..You are not root user $N"
-    exit1
+exit1
 fi
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
         echo -e "$R $2....FAILED.$N"
-        exit1
+    exit1
     else    
         echo -e "$G $2....SUCCESSFUL. $N"
     fi
 }
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
+cp mongo.repo /etc/yum.repos.d/mongo.repo 
 
 VALIDATE $? "copyin gmongo.repo"
 
-yum install mongodb-org -y &>>$LOG_FILE
+yum install mongodb-org -y 
 VALIDATE $? "install mongodb-org"
 
-systemctl enable mongod &>>$LOG_FILE
+systemctl enable mongod 
 VALIDATE $? "enable mongod"
 
-systemctl start mongod &>>$LOG_FILE
+systemctl start mongod 
 VALIDATE $? "starting mongod"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOG_FILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf 
 VALIDATE $? "127.0.0.1 to 0.0.0.0 changed"
 
-systemctl restart mongod &>>$LOG_FILE
+systemctl restart mongod 
 VALIDATE $? "restarting mongod"
