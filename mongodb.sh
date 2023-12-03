4a996267e804b38d06c32a3e775abaaf892ba68b
Roman Reings <Rajkumar>
2023-12-03 10:28:27 +0530

    added mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 DATE=$(date +%x)
+USER_ID=$(id)
+LOG_FILE=/tmp/$0-$DATE.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+if [ $USER_ID -ne 0 ]
+then
+    echo -e "$R ERROR..You are not root user $N"
+fi
+VALIDATE(){
+    if [$1 -ne 0 ]
+    then
+        echo -e "$R $2....FAILED.$N"
+    else    
+        echo -e "$G $2....SUCCESSFUL. $N"
+}
 
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
 
+VALIDATE $? "copyin gmongo.repo"
 
+yum install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "install mongodb-org"
 
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "enable mongod"
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "starting mongod"
 
-yum install mongodb-org -y
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOG_FILE
+VALIDATE $? "127.0.0.1 to 0.0.0.0 changed"
 
-systemctl enable mongod
-
-systemctl start mongod
-
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-
-systemctl restart mongod
+systemctl restart mongod &>>$LOG_FILE
+VALIDATE $? "restarting mongod"
