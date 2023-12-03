4cea69a3f7f702686114166786eb9e30050b88e2
Roman Reings <Rajkumar>
2023-12-03 10:06:53 +0530

    added mongodb

---
+++ mongodb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+DATE=$(date +%x)
+
+
+
+
+
+copy /home/centos/Shell/mongo.repo /etc/yum.repos.d/mongo.repo
+
+yum install mongodb-org -y
+
+systemctl enable mongod
+
+systemctl start mongod
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+
+systemctl restart mongod
